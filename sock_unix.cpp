f4a889e7b28beac812d1260702ff968713e88689
Shon <62799691+shonqwezon@users.noreply.github.com>
2020-05-13 22:50:04 +0500

    Release v1.0.2

--- sock_unix.cpp
+++ sock_unix.cpp
@@ -1,4 +1,58 @@
-﻿#include <iostream>
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
+using std::string;
+using std::cout;
+using std::endl;
+
+string read_(tcp::socket& socket) {
+    boost::asio::streambuf buf;
+    boost::asio::read_until(socket, buf, "\n");
+    string data = boost::asio::buffer_cast<const char*>(buf.data());
+    return data;
+}
+void send_(tcp::socket& socket, const string& message) {
+    const string msg = message + "\n";
+    boost::asio::write(socket, boost::asio::buffer(message));
+}
+
+int main() {
+    std::stringstream response;
+    std::stringstream response_body;
+
+    response_body << "<title>Test C++ HTTP Server</title>\n"
+        << "<h1>Test page</h1>\n"
+        << "<p>This is body of the test page...</p>\n"
+        << "<h2>Request headers</h2>\n"
+        << "<pre>" << "\0" << "</pre>\n"
+        << "<em><small>Test C++ Http Server</small></em>\n";
+
+    // Формируем весь ответ вместе с заголовками
+    response << "HTTP/1.1 200 OK\r\n"
+        << "Version: HTTP/1.1\r\n"
+        << "Content-Type: text/html; charset=utf-8\r\n"
+        << "Content-Length: " << response_body.str().length()
+        << "\r\n\r\n"
+        << response_body.str();
+
+    boost::asio::io_service io_service;
+    //listen for new connection
+    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 8383));
+    //socket creation 
+    tcp::socket socket_(io_service);
+    //waiting for connection
+    acceptor_.accept(socket_);
+    //read operation
+    string message = read_(socket_);
+    cout << message << endl;
+    //write operation
+    send_(socket_, response.str().c_str());
+    cout << "Servent sent Hello message to Client!" << endl;
+    return 0;
+}
+=======
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
